424039fba822e1dac3c6bcc43e6b9c394b842cae
Alex Boisvert <alex.boisvert@gmail.com>
2009-12-14 09:51:50 -0800

    Add StopwatchGroups object

--- core/src/main/scala/stopwatch/StopwatchGroup.scala
+++ core/src/main/scala/stopwatch/StopwatchGroup.scala
@@ -1,15 +1,15 @@
 /*
  *  Copyright 2009-2010 Alex Boisvert
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -19,6 +19,14 @@ package stopwatch
 import stopwatch.impl.DisabledStopwatch
 import stopwatch.impl.EnabledStopwatch
 import stopwatch.impl.StopwatchStatisticImpl
+
+object StopwatchGroups {
+  @volatile
+  private[stopwatch] var _groups = Map[String, StopwatchGroup]()
+
+  /** Returns a map of all known stopwatch groups */
+  def groups: Iterator[StopwatchGroup] = _groups.values
+}
 
 /**
  * Stopwatch group: used to initialize, start, dispose and reset stopwatches.
@@ -36,12 +44,14 @@ class StopwatchGroup(val name: String) {
 
   /** Time statistics. */
   // using j.u.c.ConcurrentHashMap because it performs better than any of the Scala collections
-  // under concurrency
-  private var _stats = new java.util.concurrent.ConcurrentHashMap[String, StopwatchStatisticImpl]()  
+  // under concurrent workloads
+  private val _stats = new java.util.concurrent.ConcurrentHashMap[String, StopwatchStatisticImpl]()
 
   private var _intervals: Array[Long] = null
 
   @volatile private var _listeners: List[String => Unit] = Nil
+
+  StopwatchGroups._groups += (name -> this)
 
   /**
    * Range for temporal hit distribution analysis.
@@ -49,15 +59,14 @@ class StopwatchGroup(val name: String) {
    * StopwatchRange(0, 30000, 200) would yield the following distribution intervals:
    * <code>
    *      0-199 ms
-   *    200-299 ms
-   *    300-399 ms
+   *    200-399 ms
+   *    400-599 ms
    *    ... ...
    *  29600-29799 ms
    *  29800-29999 ms
-   *  30000+
    * </code>
    */
-  @volatile var range: StopwatchRange = null 
+  @volatile var range: StopwatchRange = null
 
   /** Measure the elapsed time spent during execution of the provided function.
    * <p>
@@ -67,21 +76,21 @@ class StopwatchGroup(val name: String) {
     val stopwatch = get(name)
     stopwatch.doWith(f)
   }
-  
+
   /** Return a named stopwatch */
-  def get(name: String): Stopwatch = {
+  private def get(name: String): Stopwatch = {
     if (!enabled) {
       return DisabledStopwatch
     }
 
-    val existing = if (enableOnDemand) create(name) else _stats.get(name)
+    val existing = if (enableOnDemand) getOrCreate(name) else _stats.get(name)
     if (existing == null || !existing.enabled) {
       DisabledStopwatch
     } else {
       new EnabledStopwatch(name, existing)
     }
   }
-  
+
   /** Return a named stopwatch and start gathering timing statistics for it. */
   def start(name: String): Stopwatch = {
     val stopwatch = get(name)
@@ -90,21 +99,23 @@ class StopwatchGroup(val name: String) {
   }
 
   /** Return a snapshot of the statistics for a given stopwatch. */
-  def snapshot(name: String): StopwatchStatistic = { 
+  def snapshot(name: String): StopwatchStatistic = {
     ifStats(name, _.snapshot, new StopwatchStatisticImpl(this, name))
   }
 
   /** Reset statistics for a given stopwatch. */
   def reset(name: String) = ifStats(name, _.reset(), {})
-  
+
   /** Reset all stopwatches. */
   def resetAll() {
-    for (s <- _stats.values.toArray) { x: StopwatchStatisticImpl => x.reset() }
+    for (s <- _stats.values.toArray.asInstanceOf[Array[StopwatchStatisticImpl]]) {
+      s.reset()
+    }
   }
 
   /** Enable a stopwatch. */
   def enable(name: String) = {
-    val stats = create(name)
+    val stats = getOrCreate(name)
     stats.enabled = true
     stats
   }
@@ -113,12 +124,17 @@ class StopwatchGroup(val name: String) {
   def disable(name: String): Unit = ifStats(name, _.enabled = false, {})
 
   /** Get the list of known stopwatches. */
-  def names: Seq[String] = synchronized {
+  def names: Seq[String] = {
     _stats.keySet.toArray.toSeq.map  { x => x.asInstanceOf[StopwatchStatisticImpl].name }
   }
 
+  /** Dispose of this stopwatch group */
+  def dispose() = {
+    StopwatchGroups._groups -= name
+  }
+
   /** Dispose of a given stopwatch. */
-  def dispose(name: String) = synchronized { _stats.remove(name) }
+  def dispose(name: String) = _stats.remove(name)
 
   /** Dispose of all stopwatches. */
   def disposeAll() = _stats.clear()
@@ -134,8 +150,8 @@ class StopwatchGroup(val name: String) {
   }
 
   /** Return existing statistics or create new statistics for given stopwatch */
-  private def create(name: String): StopwatchStatisticImpl = synchronized {
-    ifStats(name, { x => x }, { 
+  private def getOrCreate(name: String): StopwatchStatisticImpl = synchronized {
+    ifStats(name, { x => x }, {
       val newStats = new StopwatchStatisticImpl(this, name)
       _stats.put(name, newStats)
       newStats
@@ -144,10 +160,10 @@ class StopwatchGroup(val name: String) {
 
   /** Add a stopwatch listener */
   def addListener(f: String => Unit): Unit = _listeners = f :: _listeners
-  
+
   /** Remove a stopwatch listener */
   def removeListener(f: String => Unit) { _listeners = _listeners filter { l => !(l eq f) } }
-  
+
   /** Notify all listeners that a Stopwatch has changed value */
   private[stopwatch] def notifyListeners(name: String) = _listeners.foreach { _(name) }
 }
